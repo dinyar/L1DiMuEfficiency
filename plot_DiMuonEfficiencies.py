8bdabbdcc319533c175aaa415cf13afbc17f7887
Dinyar Rabady <dinyar.rabady@cern.ch>
2015-02-26 17:21:49 +0100

    Added rate stack plots.

--- plot_DiMuonEfficiencies.py
+++ plot_DiMuonEfficiencies.py
@@ -33,13 +33,13 @@ efficiencyList.append(["mu2_recoPt", 100, 0, 100, "pT2_reco", cutDict["diMu-gmtP
 # TODO: Add invariant mass calculation.
 # TODO: Add 2-D hist showing efficiency for pT of both muons.
 
-effHistList = []
-effHistList.append(["mu1_recoEta", 100, -2.8, 2.8, "Eta1_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
-effHistList.append(["mu2_recoEta", 100, -2.8, 2.8, "Eta2_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu2"]])
-effHistList.append(["mu1_recoPhi", 100, -3.2, 3.2, "Phi1_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
-effHistList.append(["mu2_recoPhi", 100, -3.2, 3.2, "Phi2_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu2"]])
-effHistList.append(["mu1_recoPt", 100, 0, 100, "pT1_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
-effHistList.append(["mu2_recoPt", 100, 0, 100, "pT2_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu2"]])
+effStackList = []
+effStackList.append(["mu1_recoEta", 100, -2.8, 2.8, "Eta1_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+effStackList.append(["mu2_recoEta", 100, -2.8, 2.8, "Eta2_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu2"]])
+effStackList.append(["mu1_recoPhi", 100, -3.2, 3.2, "Phi1_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+effStackList.append(["mu2_recoPhi", 100, -3.2, 3.2, "Phi2_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu2"]])
+effStackList.append(["mu1_recoPt", 100, 0, 100, "pT1_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+effStackList.append(["mu2_recoPt", 100, 0, 100, "pT2_reco", cutDict["diMu-gmtPt1"], cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu2"]])
 
 
 rateList = []
@@ -62,6 +62,15 @@ rateList.append(["mu2_recoPhi", 100, -3.2, 3.2, "Phi2_reco", cutDict["diMu-recoP
 rateList.append(["mu1_recoPhi", 100, -3.2, 3.2, "Phi1_reco", cutDict["diMu-gmtPt1"]])
 rateList.append(["mu2_recoPhi", 100, -3.2, 3.2, "Phi2_reco", cutDict["diMu-gmtPt1"]])
 
+rateStackList = []
+rateStackList.append(["mu1_recoPt", 100, 0, 100, "pT1_reco", cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+rateStackList.append(["mu2_recoPt", 100, 0, 100, "pT2_reco", cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+rateStackList.append(["mu1_recoEta", 100, -2.8, 2.8, "Eta1_reco", cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+rateStackList.append(["mu2_recoEta", 100, -2.8, 2.8, "Eta2_reco", cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+rateStackList.append(["mu1_recoPhi", 100, -3.2, 3.2, "Phi1_reco", cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+rateStackList.append(["mu2_recoPhi", 100, -3.2, 3.2, "Phi2_reco", cutDict["diMu-recoPt1"], stackCutDict["subsystems_mu1"]])
+
+
 f = TFile.Open("DiMuNtuple.root")
 
 ntuple = f.Get("ntuple")
@@ -69,8 +78,11 @@ ntuple = f.Get("ntuple")
 for varList in efficiencyList:
     generateEfficiencyHist(varList)
 
-for varList in effHistList:
+for varList in effStackList:
     generateEfficiencyStack(varList)
 
 for varList in rateList:
     generateRateHist(varList)
+
+for varList in rateStackList:
+    generateRateStack(varList)
