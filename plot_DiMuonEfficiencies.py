3cdd7d665be8150bcf468155f3e3b4f009175b4d
Dinyar Rabady <dinyar.rabady@cern.ch>
2016-01-12 20:40:03 +0100

    First attempt to mimic uGMT cancel-out better.

--- plot_DiMuonEfficiencies.py
+++ plot_DiMuonEfficiencies.py
@@ -8,14 +8,56 @@ from CreateHistograms import *
 gROOT.Reset()
 gROOT.SetBatch(kTRUE)
 
+# Cancel-out requirement for sector/wedge configuration
+cancel_reqs = []
+# Same track finder. Possibiliies:
+# 1. Tivially neighbours
+# 2. At wrap-around edge
+# 2.a. Barrel
+# 2.b. Overlap/endcap
+cancel_reqs.append("((tfType1 == tfType2) &&\
+                     ((abs(tfProcessor1-tfProcessor2) == 1) ||\
+                      ((tfType1 == 0) &&\
+                       (((tfProcessor1 == 0) && (tfProcessor2 == 11)) ||\
+                        ((tfProcessor1 == 11) && (tfProcessor2 == 0)))) ||\
+                      ((tfType1 > 0) &&\
+                       (((tfProcessor1 == 0) && (tfProcessor2 == 11)) ||\
+                        ((tfProcessor1 == 11) && (tfProcessor2 == 0))))\
+                     ))")
+# Different track finders, endcap/overlap. Possibilities:
+# 1. Trivially neighbours
+# 2. At wrap around edge
+cancel_reqs.append("((((tfType1 == 1) && (tfType2 == 2)) ||\
+                      ((tfType1 == 2) && (tfType2 == 1))) &&\
+                     ((abs(tfProcessor1-tfProcessor2) < 2) ||\
+                      (((tfProcessor1 == 0) && (tfProcessor2 == 5)) ||\
+                       ((tfProcessor1 == 5) && (tfProcessor2 == 0)))\
+                     ))")
+# Different track finders, barrel/overlap. Possibilities:
+# 1. Trivially beighbours
+# 2. At wrap around edge
+cancel_reqs.append("((((tfType1 == 0) && (tfType2 == 1)) &&\
+                        (((2*tfProcessor2-1) <= tfProcessor1) &&\
+                         ((2*tfProcessor2+2) >= tfProcessor1))) ||\
+                      (((tfType1 == 1) && (tfType2 == 0)) &&\
+                       (((2*tfProcessor1-1) <= tfProcessor2) &&\
+                        ((2*tfProcessor1+2) >= tfProcessor2)))\
+                    )")
+
+cancel_requirement = ' || '.join(cancel_reqs)
+cancel_requirement = '(' + cancel_requirement + ')'
+
 # Cut dicts
-gmtCuts = {}
 genCuts = {}
+genCuts["diMu-pt1"] = ["((pT1_gen > 1) && (pT2_gen > 1))", "diMu-pt1"]
+gmtCuts = {}
 gmtCuts["diMu-pt1"] = ["((pT1 > 1) && (pT2 > 1))", "diMu-pt1"]
 # TODO: Try different distance cut for each track finder/area.
-gmtCuts["diMu-pt1_separated"] = ["((pT1 > 1) && (pT2 > 1) && (sqrt((eta1-eta2)**2+(phi1-phi2)**2) > 0.1))", "diMu-pt1_separated"]
+# TODO: Try different distance cuts for phi/eta
+gmtCuts["diMu-pt1_separatedFar"] = ["((pT1 > 1) && (pT2 > 1) && (sqrt((eta1-eta2)**2+(phi1-phi2)**2) > 0.1))", "diMu-pt1_separatedFar"]
+gmtCuts["diMu-pt1_separatedFarFix"] = ["((pT1 > 1) && (pT2 > 1) && !((sqrt((eta1-eta2)**2+(phi1-phi2)**2) < 0.1) && " + cancel_requirement + ")", "diMu-pt1_separatedFarFix"]
+gmtCuts["diMu-pt1_separatedNear"] = ["((pT1 > 1) && (pT2 > 1) && (sqrt((eta1-eta2)**2+(phi1-phi2)**2) > 0.01))", "diMu-pt1_separatedNear"]
 # TODO: Have cuts to only look at certain track finders
-genCuts["diMu-pt1"] = ["((pT1_gen > 1) && (pT2_gen > 1))", "diMu-pt1"]
 
 efficiencyList = []
 # TODO: Axis labels, think about more descriptive title.
@@ -67,26 +109,38 @@ ntuple_files = []
 ntuple_files.append("GMTDimuonNtuple.root")
 ntuple_files.append("uGMTDimuonNtuple.root")
 ntuple_files.append("uGMTDimuonNtuple.root")
+ntuple_files.append("uGMTDimuonNtuple.root")
+ntuple_files.append("uGMTDimuonNtuple.root")
 ntuple_names = []
 ntuple_names.append("gmt_ntuple")
+ntuple_names.append("ugmt_ntuple")
+ntuple_names.append("ugmt_ntuple")
 ntuple_names.append("ugmt_ntuple")
 ntuple_names.append("ugmt_ntuple")
 datasets = []
 datasets.append("GMT")
 datasets.append("uGMT")
 datasets.append("uGMT")
+datasets.append("uGMT")
+datasets.append("uGMT")
 distribution_labels = []
 distribution_labels.append(["Gen muons", "GMT muons"])
 distribution_labels.append(["Gen muons", "uGMT muons"])
-distribution_labels.append(["Gen muons", "uGMT muons w/ cancel-out"])
+distribution_labels.append(["Gen muons", "uGMT muons w/ cancel-out loose"])
+distribution_labels.append(["Gen muons", "uGMT muons w/ cancel-out loose, w/ wedges"])
+distribution_labels.append(["Gen muons", "uGMT muons w/ cancel-out tight"])
 line_colours = []
 line_colours.append(38)
 line_colours.append(46)
 line_colours.append(30)
+line_colours.append(1)
+line_colours.append(8)
 gmt_cuts = []
 gmt_cuts.append(gmtCuts["diMu-pt1"])
 gmt_cuts.append(gmtCuts["diMu-pt1"])
-gmt_cuts.append(gmtCuts["diMu-pt1_separated"])
+gmt_cuts.append(gmtCuts["diMu-pt1_separatedFar"])
+gmt_cuts.append(gmtCuts["diMu-pt1_separatedFarFix"])
+gmt_cuts.append(gmtCuts["diMu-pt1_separatedNear"])
 
 for varList in efficiencyList:
     generateCombinedEfficiencyHist(varList, ntuple_files, datasets,
