f4b153559ed79962aed6cfb5ab4be92b2874e86b
Dinyar Rabady <dinyar.rabady@cern.ch>
2016-01-12 17:13:39 +0100

    Following the changes made to the helper script.

--- plot_DiMuonEfficiencies.py
+++ plot_DiMuonEfficiencies.py
@@ -25,122 +25,70 @@ efficiencyList = []
 efficiencyList.append([["deltaEta_gen", ""],
                        binningDict["distNarrow"],
                        "abs(eta1_gen-eta2_gen)",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["deltaPhi_gen", ""],
                        binningDict["distNarrow"],
                        "abs(phi1_gen-phi2_gen)",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["deltaR_gen", ""],
                        binningDict["distNarrow"],
                        "sqrt((eta1_gen-eta2_gen)**2+(phi1_gen-phi2_gen)**2)",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["mu1_genEta", ""],
                        binningDict["etaFine"], "eta1_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["mu2_genEta", ""],
                        binningDict["etaFine"], "eta2_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["mu1_genPhi", ""],
                        binningDict["phiFine"], "phi1_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["mu2_genPhi", ""],
                        binningDict["phiFine"], "phi2_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["mu1_genPt", ""],
                        binningDict["ptFine"], "pT1_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["mu2_genPt", ""],
                        binningDict["ptFine"], "pT2_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["jPsi_genEta", ""],
                        binningDict["etaFine"], "eta_jpsi",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["jPsi_genPhi", ""],
                        binningDict["phiFine"], "phi_jpsi",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
 efficiencyList.append([["jPsi_genPt", ""],
                        binningDict["ptFine"], "pT_jpsi",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1"],
                        genCuts["diMu-pt1"]])
-
-# Basic coordinate-based cancel-out
-efficiencyList.append([["deltaEta_gen", ""],
-                       binningDict["distNarrow"],
-                       "abs(eta1_gen-eta2_gen)",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["deltaPhi_gen", ""],
-                       binningDict["distNarrow"],
-                       "abs(phi1_gen-phi2_gen)",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["deltaR_gen", ""],
-                       binningDict["distNarrow"],
-                       "sqrt((eta1_gen-eta2_gen)**2+(phi1_gen-phi2_gen)**2)",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["mu1_genEta", ""],
-                       binningDict["etaFine"],
-                       "eta1_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["mu2_genEta", ""],
-                       binningDict["etaFine"],
-                       "eta2_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["mu1_genPhi", ""],
-                       binningDict["phiFine"],
-                       "phi1_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["mu2_genPhi", ""],
-                       binningDict["phiFine"],
-                       "phi2_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["mu1_genPt", ""],
-                       binningDict["ptFine"],
-                       "pT1_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-efficiencyList.append([["mu2_genPt", ""],
-                       binningDict["ptFine"],
-                       "pT2_gen",
-                       gmtCuts["diMu-pt1"], gmtCuts["diMu-pt1_separated"],
-                       genCuts["diMu-pt1"]])
-# TODO: Add invariant mass calculation.
-
 
 # TODO: Get these via argparse
 ntuple_files = []
 ntuple_files.append("GMTDimuonNtuple.root")
 ntuple_files.append("uGMTDimuonNtuple.root")
+ntuple_files.append("uGMTDimuonNtuple.root")
 ntuple_names = []
 ntuple_names.append("gmt_ntuple")
+ntuple_names.append("ugmt_ntuple")
 ntuple_names.append("ugmt_ntuple")
 datasets = []
 datasets.append("GMT")
 datasets.append("uGMT")
+datasets.append("uGMT")
 distribution_labels = []
 distribution_labels.append(["Gen muons", "GMT muons"])
 distribution_labels.append(["Gen muons", "uGMT muons"])
+distribution_labels.append(["Gen muons", "uGMT muons w/ cancel-out"])
 line_colours = []
 line_colours.append(38)
 line_colours.append(46)
+line_colours.append(30)
+gmt_cuts = []
+gmt_cuts.append(gmtCuts["diMu-pt1"])
+gmt_cuts.append(gmtCuts["diMu-pt1"])
+gmt_cuts.append(gmtCuts["diMu-pt1_separated"])
 
 for varList in efficiencyList:
     generateCombinedEfficiencyHist(varList, ntuple_files, datasets,
                                    ntuple_names, distribution_labels,
-                                   line_colours)
+                                   line_colours, gmt_cuts)
