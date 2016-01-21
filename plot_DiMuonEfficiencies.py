d4f71323219b56fd97339db6cb0d99f21441637b
Dinyar Rabady <dinyar.rabady@cern.ch>
2016-01-21 15:15:10 +0100

    Some fixes to cuts.

--- plot_DiMuonEfficiencies.py
+++ plot_DiMuonEfficiencies.py
@@ -14,10 +14,12 @@ genCuts["mu-pt1"] = ["(pT1_gen > 1)", "mu-ptGen1"]
 genCuts["L1GenMu-pt1"] = ["((pT1_gen > 1) && (pT1 > 1))", "mu-ptL1Gen1"]
 genCuts["diMu-pt1"] = ["((pT1_gen > 1) && (pT2_gen > 1))", "diMu-ptGen1"]
 gmtCuts = {}
-gmtCuts["diMu-pt1_pTmin"] = ["((pT1_pt > 1) && (pT2_pt > 1))",
-                             "diMu-pt1_pTmin"]
-gmtCuts["diMu-pt1_qMax"] = ["((pT1_q > 1) && (pT2_q > 1))",
-                            "diMu-pt1_qMax"]
+gmtCuts["gmt_diMu-pt1"] = ["((pT1 > 1) && (pT2 > 1))",
+                           "diMu-pt1"]
+gmtCuts["ugmt_diMu-pt1_pTmin"] = ["((pT1_pt > 1) && (pT2_pt > 1))",
+                                  "diMu-pt1_pTmin"]
+gmtCuts["ugmt_diMu-pt1_qMax"] = ["((pT1_q > 1) && (pT2_q > 1))",
+                                 "diMu-pt1_qMax"]
 
 gmtCuts["bmtf"] = ["(tfType1==0)", "bmtf"]
 gmtCuts["omtf"] = ["(tfType1==1)", "omtf"]
@@ -80,6 +82,7 @@ ntuple_names.append("ugmt_ntuple")
 ntuple_names.append("ugmt_ntuple")
 ntuple_names.append("ugmt_ntuple")
 ntuple_names.append("ugmt_ntuple")
+ntuple_names.append("ugmt_ntuple")
 labels = []
 labels.append(["Gen muons", "GMT muons", "GMT"])
 labels.append(["Gen muons", "uGMT muons", "uGMT"])
@@ -92,12 +95,14 @@ labels.append(["Gen muons", "uGMT muons w/ cancel-out #DeltaR<0.5", "uGMT",
 labels.append(["Gen muons", "uGMT muons w/ cancel-out #DeltaR<0.01", "uGMT",
                "dR0-01"])
 line_colours = []
-line_colours.append(38)
+line_colours.append(1)
 line_colours.append(46)
 line_colours.append(30)
-line_colours.append(1)
+line_colours.append(38)
 line_colours.append(8)
+line_colours.append(28)
 cuts = []
+cuts.append(gmtCuts["gmt_diMu-pt1"])
 cuts.append(gmtCuts["diMu-pt1_pTmin"])
 cuts.append(gmtCuts["diMu-pt1_pTmin"])
 cuts.append(gmtCuts["diMu-pt1_pTmin"])
