3569a2c10980eba1151c42cc7fcf682584ef43d3
Dinyar Rabady <dinyar.rabady@cern.ch>
2016-03-28 22:35:25 +0200

    Small fix.

--- DiMuEfficiencyNtuplizer.py
+++ DiMuEfficiencyNtuplizer.py
@@ -28,7 +28,7 @@ elif NgenMu == 2:
 cutList = []
 cutList.append(["", None, None, None, None])
 cutList.append(["-dR0_3", 5*[0.3], 5*[[1]], 5*[1], 5*[False]])
-cutList.append(["-dR0_2", 5*[0.1], 5*[[1]], 5*[1], 5*[False]])
+cutList.append(["-dR0_2", 5*[0.2], 5*[[1]], 5*[1], 5*[False]])
 cutList.append(["-dR0_1", 5*[0.1], 5*[[1]], 5*[1], 5*[False]])
 cutList.append(["-dR0_05", 5*[0.05], 5*[[1]], 5*[1], 5*[False]])
 # cutList.append(["-dPhi0_05dEta0_1-BOMTF_dEtaFine0_1-dEtaCoarse0_3-EOMTF_dEta0_1-EMTF_dEta0_05",
