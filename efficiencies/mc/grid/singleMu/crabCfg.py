021a3efb2cdf99730f980cfe758e05482adb44c3
Dinyar Rabady <dinyar@rabady.at>
2016-10-27 13:02:27 +0200

    Added grid config files and readme.

---
+++ efficiencies/mc/grid/singleMu/crabCfg.py
@@ -0,0 +1,30 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+requestName = 'L1Ntuple-SingleMuMinusFlatPt3To70_EtaPhiRestricted'
+dataset     = '/SingleMuMinusFlatPt3To70_EtaPhiRestricted/RunIISpring16DR80-NoPURAW_NZS_withHLT_80X_mcRun2_asymptotic_v14-v1/GEN-SIM-RAW'
+splitting   = 'FileBased'
+output      = '/store/user/dinyar/cancel_out_studies/'
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = requestName
+config.General.workArea = 'crab_projects'
+
+config.section_('JobType')
+config.JobType.psetName = '../../l1Ntuple_RAW2DIGI.py'
+config.JobType.pluginName = 'Analysis'
+
+config.section_('Data')
+config.Data.inputDataset = dataset
+config.Data.inputDBS = 'global'
+config.Data.splitting = splitting
+config.Data.useParent = False
+config.Data.unitsPerJob = 2
+config.Data.outLFNDirBase = output
+config.Data.ignoreLocality = False
+
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+
