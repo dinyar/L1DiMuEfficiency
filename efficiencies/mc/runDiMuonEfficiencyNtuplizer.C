a8b78319d5685e39b61223a6d08f1d572a574388
Dinyar Rabady <dinyar@rabady.at>
2016-12-05 19:09:05 +0100

    New targets for run scripts.

--- efficiencies/mc/runDiMuonEfficiencyNtuplizer.C
+++ efficiencies/mc/runDiMuonEfficiencyNtuplizer.C
@@ -1,4 +1,4 @@
 {
   gROOT->ProcessLine(
-      ".x DiMuonEfficiencyNtuplizer.C(\"file_list_jPsi\", 2, \"20161201_defaultTuning\")");
+      ".x DiMuonEfficiencyNtuplizer.C(\"file_list_jPsi\", 2, \"20161205_defaultTuning\")");
 }
--- efficiencies/mc/runDiMuonGhostingNtuplizer.C
+++ efficiencies/mc/runDiMuonGhostingNtuplizer.C
@@ -1,4 +1,4 @@
 {
   gROOT->ProcessLine(
-      ".x DiMuonEfficiencyNtuplizer.C(\"file_list_singleMu\", 1, \"20161201_defaultTuning\")");
+      ".x DiMuonEfficiencyNtuplizer.C(\"file_list_singleMu\", 1, \"20161205_defaultTuning\")");
 }
